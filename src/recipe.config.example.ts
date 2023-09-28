4378fdda0e1718478c6bd2417142f0925861db97
kos-dw <90103467+kos-dw@users.noreply.github.com>
2023-09-29 02:22:56 +0900

    docs(README.md): READMEの追加

--- src/recipe.config.example.ts
+++ src/recipe.config.example.ts
@@ -22,10 +22,10 @@ export default class recipe {
   static search_url = ["about", "recruit"];
   /** リソースを収集するページの各要素のセレクタ */
   static selector = {
-    /** 索敵ページのタイトルを取得する要素 */
-    title: "h1",
-    /** 遷移先でのタイトルを取得する要素 */
+    /** 索敵ページで遷移先を特定するアンカー要素 */
     anchor: "header a",
+    /** 遷移先でのタイトルを取得する要素 */
+    title: "h1",
     /** 遷移先でのリソースを収集する要素 */
     items: "main img",
   };
